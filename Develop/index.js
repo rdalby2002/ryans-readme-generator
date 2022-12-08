a70ee6a70aa26208e47bdabafb82a6bed4382105
rdalby2002 <ryancdalby@gmail.com>
2022-12-08 17:31:13 -0600

    Created prompt questions to be used with inquirer in init() function to then create README.md.

--- Develop/index.js
+++ Develop/index.js
@@ -1,7 +1,54 @@
 // TODO: Include packages needed for this application
-
+const inquirer = require('inquirer');
+const fs = require('fs');
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = [
+  {
+    type: 'input',
+    message: 'Please provide a brief description of your project.',
+    name: 'description',
+  },
+  {
+    type: 'input',
+    message: 'Please provide installation instructions for your project, if any.',
+    name: 'installation',
+  },
+  {
+    type: 'input',
+    message: 'Please provide usage information for your project.',
+    name: 'usage',
+  },
+  {
+    type: 'input',
+    message: 'What license is your project under?',
+    name: 'license',
+  },
+  {
+    type: 'input',
+    message: 'Please specify contribution preferences for your project',
+    name: 'contributing',
+  },
+  {
+    type: 'input',
+    message: 'Please provide test instructions for your project, if any.',
+    name: 'tests',
+  },
+  {
+    type: 'input',
+    message: 'What is your Github username?',
+    name: 'github',
+  },
+  { 
+    type: 'input',
+    message: 'What is your email address?',
+    name: 'email',
+  },
+  {
+    type: 'input',
+    message: 'How should people reach you with questions regarding your project?',
+    name: 'questions',
+  },
+]; 
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {}
