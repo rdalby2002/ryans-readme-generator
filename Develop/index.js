8577352df43f0a591dfb1a52a8ab015061d4a3b1
rdalby2002 <ryancdalby@gmail.com>
2022-12-13 14:12:20 -0600

    Brought all previous commits together using fs.writeFile and inquirer to make a fully functioning README.md generator.

--- Develop/index.js
+++ Develop/index.js
@@ -1,67 +1,83 @@
 // TODO: Include packages needed for this application
 const inquirer = require('inquirer');
 const fs = require('fs');
-const markdown = require('./utils/generateMarkdown');
+const generateMarkdown = require('./utils/generateMarkdown')
+
 // TODO: Create an array of questions for user input
 const questions = [
   {
     type: 'input',
+    name: 'title',
     message: 'Please enter your project title.',
-    name: 'title'
   },
   {
     type: 'input',
-    message: 'Please provide a brief description of your project.',
     name: 'description',
+    message: 'Please provide a brief description of your project.',
   },
   {
     type: 'input',
-    message: 'Please provide installation instructions for your project, if any.',
     name: 'installation',
+    message: 'Please provide installation instructions for your project, if any.',
   },
   {
     type: 'input',
-    message: 'Please provide usage information for your project.',
     name: 'usage',
+    message: 'Please provide usage information for your project.',
   },
   {
     type: 'list',
     message: 'What license is your project under? (Use arrow keys to navigate between choices)',
-    choices: ['No license', 'Apache 2.0 License', 'BSD 3-Clause License', 'BSD 2-Clause License', 'Boost Software License 1.0', 'CC0', 'Attribution 4.0 International', 'Attribution-ShareAlike 4.0 International', 'Attribution-NonCommercial 4.0 International', 'Attribution-NoDerivates 4.0 International', 'Attribution-NonCommmercial-ShareAlike 4.0 International', 'Attribution-NonCommercial-NoDerivatives 4.0 International', 'Eclipse Public License 1.0', 'GNU GPL v3', 'GNU GPL v2', 'GNU AGPL v3', 'GNU LGPL v3', 'GNU FDL v1.3', 'The Hippocratic License 2.1', 'The Hippocratic License 3.0', 'IBM Public License Version 1.0', 'ISC License (ISC)', 'The MIT License', 'Mozilla Public License 2.0', 'Attribution License (BY)', 'Open Database License (ODbL)', 'Public Domain Dedication and License (PDDL)', 'The Perl License', 'The Artistic License 2.0', 'SIL Open Font License 1.1', 'The Unlicense', 'The Do What the F*ck You Want to Public License', 'The zlib/libpng License'],
     name: 'license',
+    choices: ['No license', 'Apache 2.0 License', 'BSD 3-Clause License', 'BSD 2-Clause License', 'Boost Software License 1.0', 'CC0', 'Attribution 4.0 International', 'Attribution-ShareAlike 4.0 International', 'Attribution-NonCommercial 4.0 International', 'Attribution-NoDerivates 4.0 International', 'Attribution-NonCommmercial-ShareAlike 4.0 International', 'Attribution-NonCommercial-NoDerivatives 4.0 International', 'Eclipse Public License 1.0', 'GNU GPL v3', 'GNU GPL v2', 'GNU AGPL v3', 'GNU LGPL v3', 'GNU FDL v1.3', 'The Hippocratic License 2.1', 'The Hippocratic License 3.0', 'IBM Public License Version 1.0', 'ISC License (ISC)', 'The MIT License', 'Mozilla Public License 2.0', 'Attribution License (BY)', 'Open Database License (ODbL)', 'Public Domain Dedication and License (PDDL)', 'The Perl License', 'The Artistic License 2.0', 'SIL Open Font License 1.1', 'The Unlicense', 'The Do What the F*ck You Want to Public License', 'The zlib/libpng License'],
   },
   {
     type: 'input',
-    message: 'Please specify contribution preferences for your project',
     name: 'contributing',
+    message: 'Please specify contribution preferences for your project',
   },
   {
     type: 'input',
-    message: 'Please provide test instructions for your project, if any.',
     name: 'tests',
+    message: 'Please provide test instructions for your project, if any.',
   },
   {
     type: 'input',
-    message: 'What is your Github username?',
     name: 'github',
+    message: 'What is your Github username?',
   },
   { 
     type: 'input',
-    message: 'What is your email address?',
     name: 'email',
+    message: 'What is your email address?',
   },
   {
     type: 'input',
-    message: 'How should people reach you with questions regarding your project?',
     name: 'questions',
+    message: 'How should people reach you with questions regarding your project?',
   },
-]; 
+]
 
 // TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+function writeToFile(fileContent) {
+  return new Promise(() => {
+    fs.writeFile('README.md', fileContent, err => {
+      err ? console.log(err) : console.log('File created!');
+    });
+  });
+};
 
 // TODO: Create a function to initialize app
-function init() {}
-
+function init() {
+  inquirer.prompt(questions)
+    .then((data) => { 
+      return generateMarkdown(data)
+    })
+    .then(data => {
+      return writeToFile(data)
+    })
+};
 // Function call to initialize app
 init();
+
+
--- Develop/utils/generateMarkdown.js
+++ Develop/utils/generateMarkdown.js
@@ -1,136 +1,207 @@
-// TODO: Create a function that returns a license badge based on which license is passed in
-// If there is no license, return an empty string
 function renderLicenseBadge(license) {
-  if (license = 'No license') {
+  if (license === 'No license') {
     return '';
-  } else if (license = 'Apache 2.0 License') {
-    return `![License](https://img.shields.io/badge/License-Apache_2.0-blue.svg)`;
-  } else if (license = 'BSD 3-Clause License') {
-    return `![License](https://img.shields.io/badge/License-BSD_3--Clause-blue.svg)`;
-  } else if (license = 'BSD 2-Clause License') {
-    return `![License](https://img.shields.io/badge/License-BSD_2--Clause-orange.svg)`;
-  } else if (license = 'Boost Software License 1.0') {
-    return `![License](https://img.shields.io/badge/License-Boost_1.0-lightblue.svg)`;
-  } else if (license = 'CC0') {
-    return `![License: CC0-1.0](https://img.shields.io/badge/License-CC0_1.0-lightgrey.svg)`;
-  } else if (license = 'Attribution 4.0 International') {
-    return `![License: CC BY 4.0](https://img.shields.io/badge/License-CC_BY_4.0-lightgrey.svg)`;
-  } else if (license = 'Attribution-ShareAlike 4.0 International') {
-    return `![License: CC BY-SA 4.0](https://img.shields.io/badge/License-CC_BY--SA_4.0-lightgrey.svg)`;
-  } else if (license = 'Attribution-NonCommercial 4.0 International') {
-    return `![License: CC BY-NC 4.0](https://img.shields.io/badge/License-CC_BY--NC_4.0-lightgrey.svg)`;
-  } else if (license = 'Attribution-NoDerivates 4.0 International') {
-    return `![License: CC BY-ND 4.0](https://img.shields.io/badge/License-CC_BY--ND_4.0-lightgrey.svg)`;
-  } else if (license = 'Attribution-NonCommmercial-ShareAlike 4.0 International') {
-    return `![License: CC BY-NC-SA 4.0](https://img.shields.io/badge/License-CC_BY--NC--SA_4.0-lightgrey.svg)`;
-  } else if (license = 'Attribution-NonCommercial-NoDerivatives 4.0 International') {
-    return `![License: CC BY-NC-ND 4.0](https://img.shields.io/badge/License-CC_BY--NC--ND_4.0-lightgrey.svg)`;
-  } else if (license = 'Eclipse Public License 1.0') {
-    return `![License](https://img.shields.io/badge/License-EPL_1.0-red.svg)`;
-  } else if (license = 'GNU GPL v3') {
-    return `![License: GPL v3](https://img.shields.io/badge/License-GPLv3-blue.svg)`;
-  } else if (license = 'GNU GPL v2') {
-    return `![License: GPL v2](https://img.shields.io/badge/License-GPL_v2-blue.svg)`;
-  } else if (license = 'GNU AGPL v3') {
-    return `![License: AGPL v3](https://img.shields.io/badge/License-AGPL_v3-blue.svg)`;
-  } else if (license = 'GNU LGPL v3') {
-    return `![License: LGPL v3](https://img.shields.io/badge/License-LGPL_v3-blue.svg)`;
-  } else if (license = 'GNU FDL v1.3') {
-    return `![License: FDL 1.3](https://img.shields.io/badge/License-FDL_v1.3-blue.svg)`;
-  } else if (license = 'The Hippocratic License 2.1') {
-    return `![License: Hippocratic 2.1](https://img.shields.io/badge/License-Hippocratic_2.1-lightgrey.svg)`;
-  } else if (license = 'The Hippocratic License 3.0') {
-    return `![License: Hippocratic 3.0](https://img.shields.io/badge/License-Hippocratic_3.0-lightgrey.svg)`;
-  } else if (license = 'IBM Public License Version 1.0') {
-    return `![License: IPL 1.0](https://img.shields.io/badge/License-IPL_1.0-blue.svg)`;
-  } else if (license = 'ISC License (ISC)') {
-    return `![License: ISC](https://img.shields.io/badge/License-ISC-blue.svg)`;
-  } else if (license = 'The MIT License') {
-    return `![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)`;
-  } else if (license = 'Mozilla Public License 2.0') {
-    return `![License: MPL 2.0](https://img.shields.io/badge/License-MPL_2.0-brightgreen.svg)`;
-  } else if (license = 'Attribution License (BY)') {
-    return `![License: Open Data Commons Attribution](https://img.shields.io/badge/License-ODC_BY-brightgreen.svg)`;
-  } else if (license = 'Open Database License (ODbL)') {
-    return `![License: ODbL](https://img.shields.io/badge/License-ODbL-brightgreen.svg)`;
-  } else if (license = 'Public Domain Dedication and License (PDDL)') {
-    return `![License: ODbL](https://img.shields.io/badge/License-PDDL-brightgreen.svg)`;
-  } else if (license = 'The Perl License') {
-    return `![License: Artistic-2.0](https://img.shields.io/badge/License-Perl-0298c3.svg)`;
-  } else if (license = 'The Artistic License 2.0') {
-    return `![License: Artistic-2.0](https://img.shields.io/badge/License-Artistic_2.0-0298c3.svg)`;
-  } else if (license = 'SIL Open Font License 1.1') {
-    return `![License: Open Font-1.1](https://img.shields.io/badge/License-OFL_1.1-lightgreen.svg)`;
-  } else if (license = 'The Unlicense') {
-    return `![License: Unlicense](https://img.shields.io/badge/license-Unlicense-blue.svg)`;
-  } else if (license = 'The Do What the F*ck You Want to Public License') {
-    return `![License: WTFPL](https://img.shields.io/badge/License-WTFPL-brightgreen.svg)`;
-  } else if (license = 'The zlib/libpng License') {
-    return `![License: Zlib](https://img.shields.io/badge/License-Zlib-lightgrey.svg)`;
+  } else if (license === 'Apache 2.0 License') {
+    return `
+
+![License](https://img.shields.io/badge/License-Apache_2.0-blue.svg)`;
+  } else if (license === 'BSD 3-Clause License') {
+    return `
+
+![License](https://img.shields.io/badge/License-BSD_3--Clause-blue.svg)`;
+  } else if (license === 'BSD 2-Clause License') {
+    return `
+
+![License](https://img.shields.io/badge/License-BSD_2--Clause-orange.svg)`;
+  } else if (license === 'Boost Software License 1.0') {
+    return `
+
+![License](https://img.shields.io/badge/License-Boost_1.0-lightblue.svg)`;
+  } else if (license === 'CC0') {
+    return `
+
+![License: CC0-1.0](https://img.shields.io/badge/License-CC0_1.0-lightgrey.svg)`;
+  } else if (license === 'Attribution 4.0 International') {
+    return `
+
+![License: CC BY 4.0](https://img.shields.io/badge/License-CC_BY_4.0-lightgrey.svg)`;
+  } else if (license === 'Attribution-ShareAlike 4.0 International') {
+    return `
+
+![License: CC BY-SA 4.0](https://img.shields.io/badge/License-CC_BY--SA_4.0-lightgrey.svg)`;
+  } else if (license === 'Attribution-NonCommercial 4.0 International') {
+    return `
+
+![License: CC BY-NC 4.0](https://img.shields.io/badge/License-CC_BY--NC_4.0-lightgrey.svg)`;
+  } else if (license === 'Attribution-NoDerivates 4.0 International') {
+    return `
+
+![License: CC BY-ND 4.0](https://img.shields.io/badge/License-CC_BY--ND_4.0-lightgrey.svg)`;
+  } else if (license === 'Attribution-NonCommmercial-ShareAlike 4.0 International') {
+    return `
+
+![License: CC BY-NC-SA 4.0](https://img.shields.io/badge/License-CC_BY--NC--SA_4.0-lightgrey.svg)`;
+  } else if (license === 'Attribution-NonCommercial-NoDerivatives 4.0 International') {
+    return `
+
+![License: CC BY-NC-ND 4.0](https://img.shields.io/badge/License-CC_BY--NC--ND_4.0-lightgrey.svg)`;
+  } else if (license === 'Eclipse Public License 1.0') {
+    return `
+
+![License](https://img.shields.io/badge/License-EPL_1.0-red.svg)`;
+  } else if (license === 'GNU GPL v3') {
+    return `
+
+![License: GPL v3](https://img.shields.io/badge/License-GPLv3-blue.svg)`;
+  } else if (license === 'GNU GPL v2') {
+    return `
+
+![License: GPL v2](https://img.shields.io/badge/License-GPL_v2-blue.svg)`;
+  } else if (license === 'GNU AGPL v3') {
+    return `
+
+![License: AGPL v3](https://img.shields.io/badge/License-AGPL_v3-blue.svg)`;
+  } else if (license === 'GNU LGPL v3') {
+    return `
+
+![License: LGPL v3](https://img.shields.io/badge/License-LGPL_v3-blue.svg)`;
+  } else if (license === 'GNU FDL v1.3') {
+    return `
+
+![License: FDL 1.3](https://img.shields.io/badge/License-FDL_v1.3-blue.svg)`;
+  } else if (license === 'The Hippocratic License 2.1') {
+    return `
+
+![License: Hippocratic 2.1](https://img.shields.io/badge/License-Hippocratic_2.1-lightgrey.svg`;
+  } else if (license === 'The Hippocratic License 3.0') {
+    return `
+
+![License: Hippocratic 3.0](https://img.shields.io/badge/License-Hippocratic_3.0-lightgrey.svg`;
+  } else if (license === 'IBM Public License Version 1.0') {
+    return `
+
+![License: IPL 1.0](https://img.shields.io/badge/License-IPL_1.0-blue.svg)`;
+  } else if (license === 'ISC License (ISC)') {
+    return `
+
+![License: ISC](https://img.shields.io/badge/License-ISC-blue.svg)`;
+  } else if (license === 'The MIT License') {
+    return `
+
+![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)`;
+  } else if (license === 'Mozilla Public License 2.0') {
+    return `
+
+![License: MPL 2.0](https://img.shields.io/badge/License-MPL_2.0-brightgreen.svg)`;
+  } else if (license === 'Attribution License (BY)') {
+    return `
+
+![License: Open Data Commons Attribution](https://img.shields.io/badge/License-ODC_BY-brightgreen.svg)`;
+  } else if (license === 'Open Database License (ODbL)') {
+    return `
+
+![License: ODbL](https://img.shields.io/badge/License-ODbL-brightgreen.svg)`;
+  } else if (license === 'Public Domain Dedication and License (PDDL)') {
+    return `
+
+![License: ODbL](https://img.shields.io/badge/License-PDDL-brightgreen.svg)`;
+  } else if (license === 'The Perl License') {
+    return `
+
+![License: Artistic-2.0](https://img.shields.io/badge/License-Perl-0298c3.svg)`;
+  } else if (license === 'The Artistic License 2.0') {
+    return `
+
+![License: Artistic-2.0](https://img.shields.io/badge/License-Artistic_2.0-0298c3.svg)`;
+  } else if (license === 'SIL Open Font License 1.1') {
+    return `
+
+![License: Open Font-1.1](https://img.shields.io/badge/License-OFL_1.1-lightgreen.svg)`;
+  } else if (license === 'The Unlicense') {
+    return `
+
+![License: Unlicense](https://img.shields.io/badge/license-Unlicense-blue.svg)`;
+  } else if (license === 'The Do What the F*ck You Want to Public License') {
+    return `
+
+![License: WTFPL](https://img.shields.io/badge/License-WTFPL-brightgreen.svg)`;
+  } else if (license === 'The zlib/libpng License') {
+    return `
+
+![License: Zlib](https://img.shields.io/badge/License-Zlib-lightgrey.svg)`;
   };
 }
+
 
 // TODO: Create a function that returns the license link
 // If there is no license, return an empty string
 function renderLicenseLink(license) {
-  if (license = 'No license') {
+  if (license === 'No license') {
     return '';
   } else {
-    return '\n- [License](#license)';
+    return `
+- [License](#license)`;
   };
 }
 
 // TODO: Create a function that returns the license section of README
 // If there is no license, return an empty string
 function renderLicenseSection(license) {
-  if (license = 'No license') {
+  if (license === 'No license') {
     return '';
   } else {
-    return `\n##License
+    return `
+  
+## License
 
-    This project is licensed under ${license}.`
+This project is licensed under ${license}.`
   };
 }
 
 // TODO: Create a function to generate markdown for README
 function generateMarkdown(data) {
-  return `# ${data.title}
+  const license = data.license;
 
-  ${renderLicenseBadge(data.license)}
+  return `# ${data.title}${renderLicenseBadge(license)}
 
-  ## Description
+## Description
 
-  ${data.description}
+${data.description}
 
-  ## Table of Contents
+## Table of Contents
 
-  - [Installation](#installation)
-  - [Usage](#usage)
-  - [Contributing](#contributing)
-  - [Tests](#tests)
-  - [Questions](#questions)${renderLicenseLink(data.license)}
+- [Installation](#installation)
+- [Usage](#usage)
+- [Contributing](#contributing)
+- [Tests](#tests)
+- [Questions](#questions)${renderLicenseLink(license)}
 
-  ## Installation
+## Installation
 
-  ${data.installation}
+${data.installation}
 
-  ## Usage
+## Usage
 
-  ${data.usage}
+${data.usage}
 
-  ## Contributing
+## Contributing
 
-  ${data.contributing}
+${data.contributing}
 
-  ## Tests
+## Tests
 
-  ${data.tests}
+${data.tests}
 
-  ## Questions
+## Questions
 
-  ${data.questions}
-  ${renderLicenseSection(data.license)}
-`;
+Github: ${data.github}
+
+Email: ${data.email}
+
+${data.questions}${renderLicenseSection(license)}
+  `
+;
 }
 
 module.exports = generateMarkdown;
+
